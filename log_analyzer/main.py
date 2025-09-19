9bbf2b2c6b396db76a77f66fbf6b195348520a98
Pi-Eatery <kam3014@proton.me>
2025-09-19 11:48:55 +0000

    Added in logic for the entrypoint and a few tests to make sure the logic was sound before going back for clarity and conciseness

--- log_analyzer/main.py
+++ log_analyzer/main.py
@@ -1,19 +1,21 @@
+import os
 import yaml
 
 def load_config(config_path):
+    current_wd = os.path.dirname(__file__)
+    change_directories = os.path.join (current_wd, config_path)
     try:
-        with open(config_path, 'r') as config_object:
+        with open(change_directories, 'r') as config_object:
             config = yaml.safe_load(config_object)
+    except yaml.YAMLError as e:
+        print(f"The .yaml file you requested seems to be malformed...\nError: {e}")
+        return None
     except FileNotFoundError as e:
         print(f"The file you requested cannot be found...\nError: {e}")
-        return None
-    except yaml.YAMLError as e:
-        print(f"The file you requested seems to be malformed...\nError: {e}")
         return None
     return config
 
 if __name__ == "__main__":
-    dict_to_print = {}
     dict_to_print = load_config('../config/config.yaml')
     print(f"The configuration settings are as follows:\n{dict_to_print}")
     load_config('../config/nonexistent.yaml')

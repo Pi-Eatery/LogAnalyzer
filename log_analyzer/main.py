c3430a8508a30029e5add482ef3d8267bce828e6
Pi-Eatery <kam3014@proton.me>
2025-09-19 10:53:15 +0000

    Fixed some typos and flawed logic statements

--- log_analyzer/main.py
+++ log_analyzer/main.py
@@ -1,19 +1,16 @@
-import PyYAML
+import yaml
 
 
 def load_config(config_path):
-    config_dict = {}
     try:
-        with open('config_path', 'r') as config_object:
+        with open(config_path, 'r') as config_object:
             config = yaml.safe_load(config_object)
-    except if not config_object as e:
+    except FileNotFoundError as e:
         print(f"The file you requested cannot be found...\nError: {e}")
         return None
-    except if yaml.YAMLError as e:
+    except yaml.YAMLError as e:
         print(f"The file you requested seems to be malformed...\nError: {e}")
         return None
-    if config:
-        config_dict.expand(config)
-        return config_dict
+        return config
     return None
 

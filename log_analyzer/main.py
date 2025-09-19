77eb3ca3370fdfcbfbfcc8e57513447ca0afa86c
Pi-Eatery <kam3014@proton.me>
2025-09-19 13:31:59 +0000

    Prettied up the output

--- log_analyzer/main.py
+++ log_analyzer/main.py
@@ -2,21 +2,23 @@ import os
 import yaml
 
 def load_config(config_path):
-    current_wd = os.path.dirname(__file__)
-    change_directories = os.path.join (current_wd, config_path)
     try:
-        with open(change_directories, 'r') as config_object:
+        with open(config_path, 'r') as config_object:
             config = yaml.safe_load(config_object)
     except yaml.YAMLError as e:
-        print(f"The .yaml file you requested seems to be malformed...\nError: {e}")
+        print(f"\nThe .yaml file you requested seems to be malformed...\nError: {e}\n\n")
         return None
     except FileNotFoundError as e:
-        print(f"The file you requested cannot be found...\nError: {e}")
+        print(f"\nThe file you requested cannot be found...\nError: {e}\n\n")
         return None
     return config
 
 if __name__ == "__main__":
-    dict_to_print = load_config('../config/config.yaml')
-    print(f"The configuration settings are as follows:\n{dict_to_print}")
-    load_config('../config/nonexistent.yaml')
-    load_config('../config/broken_config.yaml')
+    current_wd = os.path.dirname(__file__)
+    config_dir_path = os.path.join(current_wd, '../config/')
+    config_file_path = os.path.join(config_dir_path, 'config.yaml')
+    dict_to_print = load_config(config_file_path)
+    load_config(os.path.join(config_dir_path, 'nonexistent'))
+    load_config(os.path.join(config_dir_path, 'broken_config.yaml'))
+
+    print(f"\nThese are the current config options:\n---\n{dict_to_print}\n\n\n")

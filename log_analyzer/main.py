1c5131c363b24c39ce6a64c86b633e1603f8488b
Pi-Eatery <kam3014@proton.me>
2025-09-19 11:11:28 +0000

    Created the entrypoint for the script in main.py

--- log_analyzer/main.py
+++ log_analyzer/main.py
@@ -1,6 +1,5 @@
 import yaml
 
-
 def load_config(config_path):
     try:
         with open(config_path, 'r') as config_object:
@@ -12,3 +11,10 @@ def load_config(config_path):
         print(f"The file you requested seems to be malformed...\nError: {e}")
         return None
     return config
+
+if __name__ == "__main__":
+    dict_to_print = {}
+    dict_to_print = load_config('../config/config.yaml')
+    print(f"The configuration settings are as follows:\n{dict_to_print}")
+    load_config('../config/nonexistent.yaml')
+    load_config('../config/broken_config.yaml')
